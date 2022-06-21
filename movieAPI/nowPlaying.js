682da3b5ebe4ef8eba05c69547d96685db055ec6
cunami <79466733+tsunamii0@users.noreply.github.com>
2022-06-21 19:11:52 +0200

    Add files via upload

---
+++ movieAPI/nowPlaying.js
@@ -0,0 +1,60 @@
+const nowPlaying = document.querySelector(".nowPlaying");
+const showMorePlaying = document.querySelector(".showMorePlaying");
+
+const baseURL0 = "https://api.themoviedb.org/3/movie/now_playing?api_key=ac611aa60fbb0355792b075ff8337fbe&language=en-US&page=1";
+
+function displayMovies(arrayName, shouldReset) {
+    if(shouldReset){
+      nowPlaying.innerHTML = ''
+    }
+  arrayName.forEach((value) => {
+    const img = document.createElement("img");
+    img.addEventListener("click", getMovie);
+    function getMovie() {
+      blocker.style.display = "block";
+      const div = document.createElement("div");
+      const h1 = document.createElement("h1");
+      const description = document.createElement("p");
+      const x = document.createElement("p");
+      x.innerHTML = "✕";
+      h1.innerHTML = `${value.title}`;
+      description.innerHTML = `${value.overview}`;
+      div.appendChild(x);
+      div.appendChild(h1);
+      div.appendChild(description);
+      div.style.backgroundImage =
+        "url(" + "https://image.tmdb.org/t/p/w500/" + value.backdrop_path + ")";
+      div.setAttribute("class", "movieShow");
+      x.setAttribute("class", "xBTN");
+      document.body.appendChild(div);
+      x.addEventListener("click", hideMov);
+      function hideMov() {
+        div.style.display = "none";
+        blocker.style.display = "none";
+      }
+    }
+    img.src = "https://image.tmdb.org/t/p/w500/" + value.poster_path;
+    nowPlaying.appendChild(img);
+  });
+}
+
+const fetchMov0 = async () => {
+  const users = await fetch(baseURL0).catch((err) => console.log(err));
+  const movies = await users.json().catch((err) => console.log(err));
+  const topEight = movies.results.slice(0, 10);
+  const topRest = movies.results.slice(10);
+  showMorePlaying.addEventListener("click", showMorePlayingMov);
+  function showMorePlayingMov() {
+    if (showMorePlaying.innerHTML == "SHOW MORE") {
+      showMorePlaying.innerHTML = "SHOW LESS";
+      displayMovies(topRest);
+    } else {
+      showMorePlaying.innerHTML = "SHOW MORE";
+      displayMovies(topEight, true);
+    }
+  }
+ displayMovies(topEight);
+};
+
+fetchMov0();
+
